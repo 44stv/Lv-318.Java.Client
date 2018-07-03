7368dd603755a0a9cc5efb1e3577f9543aaeccae
OleksiiDemidov <demidov.a@me.com>
2018-07-03 11:33:16 +0300

    Add heatmap diagram instead of two diagrams by stops and by hours

--- src/app/components/transit/components/busy-stops-diagram/busy-stops-diagram.component.ts
+++ src/app/components/transit/components/busy-stops-diagram/busy-stops-diagram.component.ts
@@ -1,50 +1,26 @@
-import { AfterViewInit, Component, Input } from '@angular/core';
-import { Chart } from 'chart.js';
-
-import { environment } from '../../../../../environments/environment';
-import { DiagramService } from '../../../../services/diagram.service';
-import { Stop } from '../../../../models/stop.model';
+import {Component, Input, OnInit} from '@angular/core';
+import {DiagramService} from '../../../../services/diagram.service';
+import {Stop} from '../../../../models/stop.model';
 
 @Component({
   selector: 'app-busy-stops-diagram',
   templateUrl: './busy-stops-diagram.component.html',
   styleUrls: ['./busy-stops-diagram.component.css']
 })
-export class BusyStopsDiagramComponent implements AfterViewInit {
+export class BusyStopsDiagramComponent implements OnInit {
   @Input() id: number;
   @Input() stopList: Stop[];
-  lineChart1;
+
+  data;
+  view = [1200];
+  public visible = false;
 
   constructor(private diagramService: DiagramService) {
   }
 
-  ngAfterViewInit(): void {
-    this.diagramService.getResults(environment.serverURL + '/feedback/byStop/' + this.id + '?stop-list=')
-      .subscribe(res => {
-        const CHART = document.getElementById('lineChart1');
-
-        this.lineChart1 = new Chart(CHART, {
-          type: 'line',
-          data: {
-            labels: Object.keys(res).map(data => data.substring(data.indexOf('=', 2) + 11, data.indexOf(', lat'))),
-            datasets: [{
-              fill: true,
-              lineTension: 0.5,
-              backgroundColor: 'rgba(75,192,192,0.4)',
-              borderColor: 'rgba(75,192,192,1)',
-              borderCapStyle: 'butt',
-              borderDash: [],
-              borderDashOffset: 0.0,
-              borderJointStyle: 'miter',
-              data: Object.values(res).concat([0, 100]).map(data => (<number>data).toPrecision(3))
-            }]
-          },
-          options: {
-            legend: {
-              display: false
-            }
-          }
-        });
-      });
+  ngOnInit(): void {
+    this.diagramService.getHeatMapData(this.id)
+      .subscribe(res => this.data = res);
+    this.visible = true;
   }
 }
