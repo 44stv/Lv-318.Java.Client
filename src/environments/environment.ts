2cc2e8f8107fc8a2a52ed587c0e2c92eaf24ccd9
Stanislav Turchynskyi <S.Turchynskyi@gmail.com>
2018-07-06 17:41:37 +0300

    update server url

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,7 +6,7 @@ export const environment = {
   production: false,
 
   // serverURL: 'http://localhost:8080',
-  serverURL: 'http://uatransport.tk',
+  serverURL: 'https://uatransport.tk',
 
   accessTokenHeader: 'Authorization',
   refreshTokenHeader: 'Refresh-token',
