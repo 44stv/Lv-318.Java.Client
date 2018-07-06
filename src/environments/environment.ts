344ac7251080114e72ad958d88d88078ee3dd3ca
vladlenonopko <vladlenonopko@gmail.com>
2018-07-06 13:15:34 +0300

    deploy

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -8,16 +8,16 @@ export const environment = {
   // serverURL: 'http://localhost:8080',
   serverURL: 'http://ec2-18-184-79-174.eu-central-1.compute.amazonaws.com',
 
-  // accessTokenHeader: 'Authorization',
-  // refreshTokenHeader: 'Refresh-token',
-  //
-  // auth: {
-  //   clientID: 'fMvXBLfJoy8yuoUoTGnTa8kI-3gEePfQ',
-  //   domain: 'uatransport.eu.auth0.com',
-  //   audience: 'http://localhost:4200',
-  //   redirect: 'http://localhost:4200/callback',
-  //   scope: 'openid profile email'
-  // }
+  accessTokenHeader: 'Authorization',
+  refreshTokenHeader: 'Refresh-token',
+
+  auth: {
+    clientID: 'fMvXBLfJoy8yuoUoTGnTa8kI-3gEePfQ',
+    domain: 'uatransport.eu.auth0.com',
+    audience: 'http://localhost:4200',
+    redirect: 'http://localhost:4200/callback',
+    scope: 'openid profile email'
+  }
 };
 
 /*
