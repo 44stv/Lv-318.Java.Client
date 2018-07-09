792e71978c4a3ecc493a4fff901570ea8c2c9bad
Vladlen Onopko <31824881+vladlenonopko@users.noreply.github.com>
2018-07-09 15:12:28 +0300

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,8 +5,8 @@
 export const environment = {
   production: false,
 
-  serverURL: 'https://localhost:8080',
-  // serverURL: 'https://uatransport.tk',
+  //serverURL: 'https://localhost:8080',
+   serverURL: 'https://uatransport.tk',
 
   accessTokenHeader: 'Authorization',
   refreshTokenHeader: 'Refresh-token',
