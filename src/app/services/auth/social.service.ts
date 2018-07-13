6f8e3a9805abf3d0d93e3451c56492f6336eb1d8
Stanislav Turchynskyi <S.Turchynskyi@gmail.com>
2018-07-13 11:48:11 +0300

    Format code

--- src/app/services/auth/social.service.ts
+++ src/app/services/auth/social.service.ts
@@ -5,18 +5,20 @@ import { User } from '../../models/user.model';
 import { environment } from '../../../environments/environment';
 
 const httpOptions = {
-  headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+  headers: new HttpHeaders({'Content-Type': 'application/json'})
 };
-@Injectable({ providedIn: 'root' })
+
+@Injectable({providedIn: 'root'})
 export class SocialService {
 
   private socialUrl = environment.serverURL + '/user/social';
 
   constructor(private http: HttpClient) {
   }
+
   signInWithSocial(user: User): Observable<any> {
     console.log('service');
-    console.log('servie'+ user.email);
+    console.log('servie' + user.email);
     return this.http.post<User>(this.socialUrl, user);
   }
 
