86ae053424d28261110752be9ec9be1c1cbe031d
RomanSitchuk <yaalex438@gmail.com>
2018-07-14 19:51:40 +0300

    change admin menu

--- src/app/components/admin/admin.component.ts
+++ src/app/components/admin/admin.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import {ExcategoryService} from '../../services/excategory.service';
+import {environment} from '../../../environments/environment';
+import {Observable} from 'rxjs';
+import {ExcategoryModel} from '../../models/excategory.model';
 
 @Component({
   selector: 'app-admin',
@@ -6,10 +10,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./admin.component.css']
 })
 export class AdminComponent implements OnInit {
+  public list: Observable<ExcategoryModel[]> = this.service.getTopCategories();
+  public cities: Observable<ExcategoryModel[]>;
+  public serverURL = environment.serverURL + '/category/img?link=';
 
-  constructor() { }
+  constructor(public service: ExcategoryService) {
+  }
 
   ngOnInit() {
+    this.list = this.service.getTopCategories();
+  }
+
+  getCities(nextLevel: String) {
+    this.cities = this.service.getCategoriesByNextLevel(nextLevel);
   }
 
 }
