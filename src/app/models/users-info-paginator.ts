74bcd818bb4031ab8fb4cba32792e85955547f71
RomanSitchuk <yaalex438@gmail.com>
2018-07-17 11:19:40 +0300

    format code

--- src/app/models/users-info-paginator.ts
+++ src/app/models/users-info-paginator.ts
@@ -1,28 +1,28 @@
-import { UserInfoModel } from './user-info.model';
+import {UserInfo} from './userInfo.model';
 
 export interface UsersInfoPaginatorPaginator {
-  content:          UserInfoModel[];
-  pageable:         PageableClass;
-  totalPages:       number;
-  totalElements:    number;
-  last:             boolean;
-  size:             number;
-  number:           number;
-  sort:             Sort;
+  content: UserInfo[];
+  pageable: PageableClass;
+  totalPages: number;
+  totalElements: number;
+  last: boolean;
+  size: number;
+  number: number;
+  sort: Sort;
   numberOfElements: number;
-  first:            boolean;
+  first: boolean;
 }
 
 export interface PageableClass {
-  sort:       Sort;
-  offset:     number;
-  pageSize:   number;
+  sort: Sort;
+  offset: number;
+  pageSize: number;
   pageNumber: number;
-  unpaged:    boolean;
-  paged:      boolean;
+  unpaged: boolean;
+  paged: boolean;
 }
 
 export interface Sort {
-  sorted:   boolean;
+  sorted: boolean;
   unsorted: boolean;
 }
