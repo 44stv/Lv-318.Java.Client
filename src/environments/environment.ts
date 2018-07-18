31014c2488b656662b6d789cb9df624d29dc8cf5
RomanSitchuk <yaalex438@gmail.com>
2018-07-18 09:01:32 +0300

    fix route to feedbackcriteria, delete unused, add missing implementations

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,8 +6,8 @@ export const environment = {
   production: false,
   clientUrl: 'http://localhost:4200/',
   imageServerURL: 'http://localhost:8090',
-  serverURL: 'http://localhost:8443',
-  // serverURL: 'https://uatransport.tk',
+  // serverURL: 'http://localhost:8080',
+  serverURL: 'https://uatransport.tk',
 
   accessTokenHeader: 'Authorization'
 };
